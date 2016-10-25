dc2aef8da86f0aa72a09483d451e61edb4694252
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-25 17:01:41 -0600

    added div tags to images

--- public_html/epic/persona1.php
+++ public_html/epic/persona1.php
@@ -2,7 +2,9 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<div class="img">
 		<link rel="stylesheet" href="../epic/css/css-epic.css" type="text/css" />
+		</div>
 	</head>
 	<body>
 		<main>
--- public_html/epic/persona2.php
+++ public_html/epic/persona2.php
@@ -9,7 +9,9 @@
 			<!-- persona section -->
 			<section>
 				<h1>Persona 2</h1>
+				<div class="img">
 				<img src="img/kitecrypt-pepe.jpg" alt="Picture of Pepe">
+				</div>
 				<h2>Pepe</h2>
 				<!-- add new p as needed -->
 				<!-- persona trait goes in the strong tags, in front of the : (e.g. <strong>Needs:</strong>) -->
