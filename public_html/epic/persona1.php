f332e001c0f4557480315b8e1ccfa2491f7710e7
Grant Wells <gwells4@cnm.edu>
2016-10-25 16:14:29 -0600

    Typed in persona for Janice.

--- public_html/epic/persona1.php
+++ public_html/epic/persona1.php
@@ -9,17 +9,27 @@
 			<!-- persona section -->
 			<section>
 				<h1>Persona</h1>
-				<h2></h2>
+				<h2>Janice</h2>
 				<!-- add new p as needed -->
 				<!-- persona trait goes in the strong tags, in front of the : (e.g. <strong>Needs:</strong>) -->
-				<p><strong>:</strong></p>
+				<p><strong>System: </strong>Desktop running Windows (Internet Explorer)</p>
+				<p><strong>Experience Level: </strong>Intermediate user. Comfortable with email and office software.</p>
+				<p><strong>Profession: </strong>Accountant, deals with sensitive data. MBA with 5 years accounting experience.</p>
+				<p><strong>Needs: </strong></p>
+				<ul>
+					<li>Send and receive sensitive data</li>
+					<li>Timely</li>
+					<li>Easy to view</li>
+					<li>Secure</li>
+					<li>Confirmation receipt</li>
+				</ul>
 			</section>
 			<!-- summary section -->
 			<section>
 				<h1>Summary</h1>
 				<!-- make new li as needed for summary items-->
 				<ul>
-					<li></li>
+					<li>Wants to mprove customer experience by providing timely and secure correspondence.</li>
 				</ul>
 			</section>
 		</main>
