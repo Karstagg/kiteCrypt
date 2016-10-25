39b269df15b322ef4e6cf16458cc3b37f06cfc42
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-25 16:02:34 -0600

    uploading html template

--- public_html/epic/persona1.php
+++ public_html/epic/persona1.php
@@ -5,7 +5,22 @@
 	</head>
 	<body>
 		<main>
-
+			<!-- persona section -->
+			<section>
+				<h1>Persona</h1>
+				<h2></h2>
+				<!-- add new p as needed -->
+				<!-- persona trait goes in the strong tags, in front of the : (e.g. <strong>Needs:</strong>) -->
+				<p><strong>:</strong></p>
+			</section>
+			<!-- summary section -->
+			<section>
+				<h1>Summary</h1>
+				<!-- make new li as needed for summary items-->
+				<ul>
+					<li></li>
+				</ul>
+			</section>
 		</main>
 	</body>
 </html>
