9c1c7d428855a5326aad463952c2dcb41ff2b990
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-25 16:34:49 -0600

    updated persona 3

--- public_html/epic/persona3.php
+++ public_html/epic/persona3.php
@@ -9,17 +9,31 @@
 			<!-- persona section -->
 			<section>
 				<h1>Persona</h1>
-				<h2></h2>
+				<h2>James</h2>
 				<!-- add new p as needed -->
 				<!-- persona trait goes in the strong tags, in front of the : (e.g. <strong>Needs:</strong>) -->
-				<p><strong>:</strong></p>
+				<p><strong>System: </strong> Mobile iOS on his iPad.</p>
+				<p><strong>Experience level: </strong>Advanced tech user. Comfortable with multiple operating systems, command line, etc.</p>
+				<p><strong>Profession: </strong>Network security specialist</p>
+				<p><strong>Likes: </strong> Encrypting everything he can</p>
+				<p><strong>Needs: </strong></p>
+				<ul>
+					<li>To communicate with peers through secure means</li>
+					<li>Privacy</li>
+					<li>dependability </li>
+					<li>zero knowledge</li>
+				</ul>
 			</section>
 			<!-- summary section -->
 			<section>
 				<h1>Summary</h1>
 				<!-- make new li as needed for summary items-->
 				<ul>
-					<li></li>
+					<li>James</li>
+					<li>Tech expert</li>
+					<li>Network security specialist</li>
+					<li>likes encryption</li>
+					<li>needs to communicate securely and reliably without fear</li>
 				</ul>
 			</section>
 		</main>
