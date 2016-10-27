f8436b8c6690fee63097041a31935679b12802d6
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-27 15:20:21 -0600

    updated persona 3 picture and updated image size css

--- public_html/epic/persona3.php
+++ public_html/epic/persona3.php
@@ -10,8 +10,9 @@
 		<main>
 			<!-- persona section -->
 			<section>
+				<img class="persona-picture" src="img/kitecrypt-james.jpg" alt="Picture of James">
 				<h1>Persona</h1>
-				<img src="img/kitecrypt-james.jpg" alt="Picture of James">
+
 
 				<h2>James</h2>
 				<!-- add new p as needed -->
