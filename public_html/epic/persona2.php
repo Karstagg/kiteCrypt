97d57794c7f42aa375800edbcede1af4562cf425
Jon Sheafe <msckj@yahoo.com>
2016-10-27 12:00:18 -0600

    Merge branch 'master' of C:\Users\finef\Desktop\bootcamp\git\kiteCrypt with conflicts.

--- public_html/epic/persona2.php
+++ public_html/epic/persona2.php
@@ -33,7 +33,10 @@
 						<li>Financial Infomation</li>
 				</ul>
 				</p>
-			</section>
+				</section>
+			<!--User Case -->
+			<h3>Use Case</h3>
+				<p>Pepe needs an easy way to receive and send sensative financial information to his accountant. Pepe is not technically savy, so he needs  an easy to use interface. Pepe logs into KiteCrypt to connect with Janice.</p>
 			<!-- summary section -->
 			<section>
 				<h1>Summary</h1>
