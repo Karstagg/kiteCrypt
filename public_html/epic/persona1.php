7c58f9bfa41ddba3dbdcc9514849020183cc9157
Grant Wells <gwells4@cnm.edu>
2016-10-27 14:55:51 -0600

    Added a use case to persona 2.

--- public_html/epic/persona1.php
+++ public_html/epic/persona1.php
@@ -28,9 +28,9 @@
 					<li>Confirmation receipt</li>
 				</ul>
 			</section>
-			<!-- summary section -->
+			<!-- use case section -->
 			<section>
-				<h1>Summary</h1>
+				<h1>Use Case</h1>
 				<!-- make new li as needed for summary items-->
 				<ul>
 					<li>Wants to improve customer experience by providing timely and secure correspondence.</li>
