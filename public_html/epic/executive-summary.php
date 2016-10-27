829f3cb0564f600d2f14a1d181ed306747c08d46
Grant Wells <gwells4@cnm.edu>
2016-10-27 11:53:07 -0600

    Added an executive summary.

---
+++ public_html/epic/executive-summary.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/css-epic.css" type="text/css" />
+		<title>kiteCrypt: Executive Summary</title>
+	</head>
+	<body>
+		<main>
+			<!-- executive summary -->
+			<p>kiteCrypt is a secure file storage and file transfer site for our insecure world. For a reasonable fee, users can get an acount on our site. Their account will allow them to store files and data on our site, as well as transfer files to others through a secure link. kiteCrypt is usable from any device with an internet connection and a web browser. Files and data may be stored on the site indefinitely, or scheduled for deletion at a specified time.</p>
+		</main>
+	</body>
+</html>
