341abd055a3670d5d36f6ca0e27166e54cf9b6d6
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-01 15:33:32 -0600

    added description of entities and attributes

--- public_html/epic/data-design.php
+++ public_html/epic/data-design.php
@@ -8,12 +8,52 @@
 	</head>
 	<body>
 		<main>
+			<h1>kiteCrypt Data Design</h1>
 			<!-- data design -->
 			<section>
-
-
+				<h2>Entities and Attributes</h2>
+				<h4>Profile *STRONG*</h4>
+				<details>
+					<summary></summary>
+					<ul>
+						<li>profileId (PRIMARY_KEY AUTO_INCREMENT)</li>
+						<li>userName (UNIQUE)</li>
+						<li>key (UNIQUE)</li>
+					</ul>
+				</details>
+				<h4>Friends *WEAK*</h4>
+				<details>
+					<summary></summary>
+					<ul>
+						<li>friendsProfileId (FOREIGN_KEY)</li>
+						<li>friendsFriendId (FOREIGN_KEY)</li>
+					</ul>
+				</details>
+				<h4>Invite *WEAK*</h4>
+				<details>
+					<summary></summary>
+					<ul>
+						<li>inviteId (FOREIGN_KEY)</li>
+						<li>invitedId (FOREIGN_KEY)</li>
+						<li>dateTime</li>
+						<li>passPhrase</li>
+					</ul>
+				</details>
+				<h4>Message *WEAK*</h4>
+				<details>
+					<summary></summary>
+					<ul>
+						<li>messageId (PRIMARY_KEY AUTO_INCREMENT)</li>
+						<li>senderId (FOREIGN_KEY)</li>
+						<li>receiverId (FOREIGN_KEY)</li>
+						<li>dateString</li>
+						<li>text</li>
+					</ul>
+				</details>
+			</section>
+			<!-- ERD -->
+			<section>
 				<img src="img/kiteCryptERD.svg" alt="kiteCrypt ERD" width="80%" height="auto">
-
 			</section>
 		</main>
 	</body>
