5a49641a0ee737b3dd08e420763b6751c03d17d3
jguzman41 <jguzman41@cnm.edu>
2016-11-03 16:56:45 -0600

    trying to fix index.php

---
+++ public_html/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" href="main-css/stylesheet.css" type="text/css">
+		<?php require_once("lib/head-util.php");?>
+	</head>
+	<body>
+		<div class="bg-image">
+			<header>
+
+			</header>
+			<!--		main content section-->
+			<main>
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="kite text-center">
+								<h1>KiteCrypt</h1>
+								<p>A secure chat site for our insecure world</p>
+							</div>
+						</div>
+					</div>
+					<!--				<img src="media/kite-logo.png" class="img-responsive img-center" alt="background-logo">-->
+
+				</div>
+			</main>
+			<footer>
+				<div class="container-fluid">
+					<div class="row text-center">
+						<div class="col-sm-6">
+							<p>Your failures are your own, old man! I say, follow throooough! Then it's time to get your beak wet tonight playa! Why are you doing this bit? We're gonna die. Meeseeks don't usually have to exist for this long. It's gettin' weeeiiird.
+
+								Then it's time to get your beak wet tonight playa! Well I don't like your unemployed genes in my grandchildren, Jerry. Oh my god, how could I not see this coming? My lust, *sob* my greed! I deserve thiiiiii- (Rick cures him) Ladies, let's get out of here. I haven't learned a thiiiiiing! Oh no. I'm late to class, bitch. Yo! What up my glip glops! I'm a bit of a stickler Meeseeks, what about your short game? The fucking amish bitch shot me! Really, you're gonna pull that move? I guided your entire civilisation. Your people have a holiday named ricksgiving. They teach kids about me in school.
+							</p>
+						</div>
+						<div class="col-sm-6">
+							<p>You gotta shove these seeds way up your butt morty, waay up there. Oh my god, how could I not see this coming? My lust, *sob* my greed! I deserve thiiiiii- (Rick cures him) Ladies, let's get out of here. I haven't learned a thiiiiiing! Is he keeping his shoulders square? Oooooooh he's tryin'! You're missing the point Morty. Why would he drive a smaller toaster with wheels? I mean, does your car look like a smaller version of your house? No.Close. It's gonna make your kidneys shut down. Get off the high road Summer. We all got pink eye because you wouldn't stop texting on the toilet. Man, that guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come.You gotta shove these seeds way up your butt morty, waay up there. Oh my god, how could I not see this coming? My lust, *sob* my greed! I deserve thiiiiii- (Rick cures him) Ladies, let's get out of here. I haven't learned a thiiiiiing! Is he keeping his shoulders square? Oooooooh he's tryin'! You're missing the point Morty. Why would he drive a smaller toaster with wheels? I mean, does your car look like a smaller version of your house? No.Close. It's gonna make your kidneys shut down. Get off the high road Summer. We all got pink eye because you wouldn't stop texting on the toilet. Man, that guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come.You gotta shove these seeds way up your butt morty, waay up there. Oh my god, how could I not see this coming? My lust, *sob* my greed! I deserve thiiiiii- (Rick cures him) Ladies, let's get out of here. I haven't learned a thiiiiiing! Is he keeping his shoulders square? Oooooooh he's tryin'! You're missing the point Morty. Why would he drive a smaller toaster with wheels? I mean, does your car look like a smaller version of your house? No.Close. It's gonna make your kidneys shut down. Get off the high road Summer. We all got pink eye because you wouldn't stop texting on the toilet. Man, that guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come.</p>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
