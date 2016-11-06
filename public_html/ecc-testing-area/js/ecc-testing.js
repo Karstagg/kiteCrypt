c7e3fd938b85b92186b26180a49504dd3eda2e22
Grant Wells <gwells4@cnm.edu>
2016-11-06 13:18:11 -0700

    Added scripts for the elliptic curve dropdown menu.

--- public_html/ecc-testing-area/js/ecc-testing.js
+++ public_html/ecc-testing-area/js/ecc-testing.js
@@ -598,7 +598,45 @@ function getN(n) {
    return typeof n == 'object' ? n : document.getElementById(n);
 }
 
+function selectEllipticCurve(ellipticCurveName) {
 
+   if (ellipticCurveName == null) {
+      ellipticCurveName = "secp160r1";
+   }
+
+   ellipticCurve = initializeEllipticCurveParameters(ellipticCurveName);
+
+}
+
+function nistP384() {
+   // p = 2^160 - 2^32 - 2^14 - 2^12 - 2^9 - 2^8 - 2^7 - 2^3 - 2^2 - 1
+   var p = fromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFAC73");
+   var a = BigInteger.ZERO;
+   var b = fromHex("7");
+   //byte[] S = null;
+   var n = fromHex("0100000000000000000001B8FA16DFAB9ACA16B6B3");
+   var h = BigInteger.ONE;
+   var curve = new ECCurveFp(p, a, b);
+   var G = curve.decodePointHex("04"
+      + "3B4C382CE37AA192A4019E763036F4F5DD4D7EBB"
+      + "938CF935318FDCED6BC28286531733C3F03C4FEE");
+   return new X9ECParameters(curve, G, n, h);
+}
+
+function nistP521() {
+// p = 2^160 - 2^32 - 2^14 - 2^12 - 2^9 - 2^8 - 2^7 - 2^3 - 2^2 - 1
+var p = fromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFAC73");
+var a = BigInteger.ZERO;
+var b = fromHex("7");
+//byte[] S = null;
+var n = fromHex("0100000000000000000001B8FA16DFAB9ACA16B6B3");
+var h = BigInteger.ONE;
+var curve = new ECCurveFp(p, a, b);
+var G = curve.decodePointHex("04"
+   + "3B4C382CE37AA192A4019E763036F4F5DD4D7EBB"
+   + "938CF935318FDCED6BC28286531733C3F03C4FEE");
+return new X9ECParameters(curve, G, n, h);
+}
 
 /*
  function () {
