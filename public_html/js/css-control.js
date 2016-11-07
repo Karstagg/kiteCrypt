3d2c5d78ad7d355decc1890be9e7afb1f99eaf8c
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-06 18:37:33 -0700

    changed   body  css to media queries so that the text does not cover  the background image  when a smartphone keyboard is  deployed

--- public_html/js/css-control.js
+++ public_html/js/css-control.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-	//this function skips the into animations on a click for kite crypt by changing the css classes for the animate elements
+	//this jQuery function skips the into animations on a click for kite crypt by changing the css classes for the animate elements
 	$('body').click(function() {
 		$('#kiteCrypt-title').removeClass("fade-in").addClass("skip-fade-in");
 		$('#sign-up').removeClass("fade-in-2").addClass("skip-fade-in-2");
