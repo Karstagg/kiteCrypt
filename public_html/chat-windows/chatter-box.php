c1bc8da719bd0783d5fb9b143de6e0cdc2a3aab4
Jonathan Guzman <jguzman41@cnm.edu>
2016-11-08 15:46:58 -0700

    Worked on creating .css and chatter-box.php

--- public_html/chat-windows/chatter-box.php
+++ public_html/chat-windows/chatter-box.php
@@ -2,6 +2,55 @@
 	<html lang="en">
 	<head>
 	<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--		Inserting Bootstrap-->
+		<!-- add CSS file -->
+		<link href="chat-stylesheet.css" rel="stylesheet" type="text/css"  />
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
-	<body></body>
+	<body>
+
+		<h1>Bootstrap Demo</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12"
+				<h2>Welcome</h2>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<p>5 more minute of this and I'm going to get mad! I am Mr. Booby Buyer. I'll buy those boobies for 25 schmeckles! Whoa, spoilers! I'm a whole season behind. Oh, we're welllll past that, Jerry!</p>
+				</div>
+				<div class="col-md-6">
+					<p>Aw, c'mon Rick. That doesn't seem so bad. My name is Slip- Slippery Stair. I'll take ya down there for 25 shmeckels! I'm sorry, Morty. It's a bummer. In reality you're as dumb as they come. And now, human music. boop beep boop... boop beep boop...</p>
+				</div>
+			</div>
+
+			<!--			This is the next content area-->
+			<div class="row"></div>
+			<div class="col-md-3">text area 1</div>
+			<div class="col-md-3">text area 2</div>
+			<div class="col-md-3">text area 3</div>
+			<div class="col-md-3">text area 4</div>
+		</div>
+	</body>
 </html>
