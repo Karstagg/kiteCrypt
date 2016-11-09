7fa7f842bfa8ff8cae98be6bec7b5867ba5e4bbb
Jon Sheafe <msckj@yahoo.com>
2016-11-09 14:35:49 -0700

    add profile class continuation https://app.asana.com/0/201603013746932/210337949745538

--- php/Profile.php
+++ php/Profile.php
@@ -1,1 +1,44 @@
-x
+<?php
+	namespace Edu\Cnm\KiteCrypt;
+	require_once("autoload.php");
+
+	/**
+	 * Profile Class creation
+	 *
+	 * @author Jon Sheafe <msckj@yahoo.com>
+	 * @version 1.0.0
+	 */
+
+class Profile implements \JsonSerializable {
+
+	/**
+	 * id for profile; this is the primary key
+	 * @var int $profileId
+	 * **/
+	private $profile;
+
+	/**
+	 * User Name for provile;
+	 * @var string
+	 * **/
+	private $profileUserName;
+
+	  /**
+		* Public Key for encryption for profile
+		*
+		* @var string
+		*/
+	  private $profilePublicKey;
+
+	/**
+	 * constructor for this Profile
+	 *
+	 * @param int|null $newProfileId  id of profile or null if a new profile.
+	 * @param string $newProfileUserName string containing user name
+	 * @param string $newProfilePublicKey string containing user public key data for encryption.
+	 *
+	 */
+
+
+
+	}
