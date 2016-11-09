3687550977c87e5a79ea2154385a7a5b0bdb9e4e
Jonathan Guzman <jguzman41@cnm.edu>
2016-11-09 09:26:21 -0700

    worked on chatter-box css

--- kitecrypt-ui/chat-windows/chatter-box.php
+++ kitecrypt-ui/chat-windows/chatter-box.php
@@ -29,7 +29,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 	<body>
-
+<!--top of page-->
 		<h1>Bootstrap Demo</h1>
 		<div class="container">
 			<div class="row">
