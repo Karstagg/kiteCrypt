6e4fff957058ffaeadc4f65149d03a5e2a9504a6
Jon Sheafe <msckj@yahoo.com>
2016-11-10 15:58:50 -0700

    Completed coding profile class https://app.asana.com/0/201603013746932/210337949745538

--- php/Profile.php
+++ php/Profile.php
@@ -38,7 +38,135 @@ class Profile implements \JsonSerializable {
 	 * @param string $newProfilePublicKey string containing user public key data for encryption.
 	 *
 	 */
-
-
-
+	public function __construct(int $newProfileId = null, string $newProfileUserName, string $newProfilePublicKey) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileUserName($newProfileUserName);
+			$this->setProfilePublicKEy($newProfilePublicKey);
+		}
+		catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		}
+		catch(\RangeException $range ) {
+			// rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}
+		catch(\TypeError $typeError) {
+			// rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		}
+		catch(\Exception $exception) {
+			// rethrow the exception to caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
 	}
+/*
+ * accessor method for profile id
+ *
+ * @return int|null value of user id
+ */
+	/**
+	 * @return int
+	 */
+	public function getProfile() {
+		return ($this->profile);
+	}
+
+	/*
+	 * mutator mehtod for profile id
+	 *
+	 * @param int|null $newProfileId new value of profile id
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newProfileId is not an integer
+	 */
+	public function setProfileId(int $newProfileId = null) {
+		// base case: if th eprofile id is null, this new profile without a mySQL assigned id (yet)
+		if($newProfileId === null) {
+			$this->profileId = null;
+			return;
+		}
+		// verify the profile id is positive
+		if($newProfileId <= 0) {
+			throw(new \RangeException("profile id is not positive"));
+		}
+		// convert and store the profile id
+		$this->profileId = $newProfileId;
+	}
+
+	/*
+	 * accessor method for profile user name
+	 *
+	 * @return string value of profile user name
+	 */
+	public function getProfileUserName() {
+		return($this->profileUserName);
+	}
+
+	/*
+	 * mutator for profile user name
+	 *
+	 * @param string $newProfileUserName new value of user name
+	 *@throws \InvalidArgumentException if $newProfileUserName is not a string or insecure
+	 * @throws \RangeException if $newProfileUserName is >20 characters
+	 * @throws \TypeError if $newProfileUserName is not a string
+	 */
+	public function setProfileUserName(string $newProfileUserName) {
+		// verify the user name is secure
+		$newProfileUserName = trim($newProfileUserName);
+		$newProfileUserName = filter_var($newProfileUserName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newProfileUserName) === true) {
+			throw(new \InvalidArgumentException("profile user name is empty or insecure"));
+		}
+
+		// verify the profile user name will fit in the database
+		if(strlen($newProfileUserName) > 20) {
+			throw(new \RangeException("profile user name too large"));
+		}
+		$this->profileUserName = $newProfileUserName;
+	}
+
+
+	/*
+	 * accessor method for profile public key
+	 *
+	 * @return string value of profile public key
+	 */
+	public function getProfilePublicKey() {
+		return($this->profilePublicKey);
+	}
+	/*
+	 * mutator method for profile public key
+	 *
+	 * @param string $newProfilePublicKey
+	 * @throws \InvalidArgumentException if $newProfilePublicKey is not a string or insecure
+	 * @throws \RangeException if $newProfilePublicKey is > 1024 characters
+	 * @throws \TypeError if $newProfilePublicKEy is not a string
+	 */
+	public function setProfilePublicKey(string $newProfilePublicKey) {
+		// verify the profile public key is secure
+		$newProfilePublicKey = trim($newProfilePublicKey);
+		$newProfilePublicKey = filter_var($newProfilePublicKey, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty(@$newProfilePublicKey) === true) {
+			throw(new \InvalidArgumentException("profile public key is empty or insecure"));
+		}
+		// verify profile public key will fit in database
+		if(strlen($newProfilePublicKey) > 1024) {
+			throw (new \RangeException("profile public key content is too large"));
+		}
+		// store the profile public key
+		$this->profilePublicKey = $newProfilePublicKey;
+	}
+	/*
+	 * formets the state variables for JSON serialization
+	 *
+	 * @return array results state variables to seialize
+	 */
+	public function jsonSerialize(){
+		$fields = get_object_vars($this);
+		return($fields);
+	}
+
+
+
+}
