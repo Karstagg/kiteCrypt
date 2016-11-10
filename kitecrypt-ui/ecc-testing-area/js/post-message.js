b69b733f31cbb30b63a9b19db41152d590f095f0
Grant Wells <gwells4@cnm.edu>
2016-11-10 16:35:49 -0700

    Added an alert telling the user that curve selection is not available 2.

--- kitecrypt-ui/ecc-testing-area/js/post-message.js
+++ kitecrypt-ui/ecc-testing-area/js/post-message.js
@@ -2,6 +2,10 @@
 var username;
 var usernameList = [];
 
+var friendName;
+var friendList = [];
+var chatFriend;
+
 var userStatus = "available";
 
 /*
@@ -67,8 +71,48 @@ function assignUsername() {
 }
 
 
+function recordFriend() {
 
-function changeStatus() {
+	friendName = getN("friendInput").value;
+
+	// Check that a friend's username was entered.
+	if (friendName == "") {
+		alert("Please enter a friend's username.");
+		return;
+	}
+
+	// Check the friend's username list, to verify
+	// that the friend's username has not already
+	// been added.
+	var numberOfFriends = friendList.length;
+	var i;
+
+	for (i = 0; i < numberOfFriends; i++) {
+		if (friendName == friendList[i]) {
+			alert("\"" + friendName + "\" has already been recorded.");
+			return;
+		}
+	}
+
+	// If the friend's username has not already been
+	// recorded, add it to the list of friends.
+	friendList.push(friendName);
+	numberOfFriends = friendList.length;
+
+	var friendListHtml = "";
+
+	for (i = 0; i < numberOfFriends; i++) {
+		friendListHtml = friendListHtml + "<span class=\"friendPresenceIconContainer\" id=\"friendPresenceIconContainer" + i + "\"></span>";
+		friendListHtml = friendListHtml + "<span class=\"friendName\" id=\"friendName" + i + "\">" + friendList[i] + "</span><br>";
+	}
+
+	// var friendListHtmlOnPage = getN("friendsList").innerHTML;
+	getN("friendList").innerHTML = friendListHtml;
+
+}
+
+
+function changeUserStatus() {
 
 	var userStatusRadios = document.getElementsByName("userStatus");
 
@@ -82,20 +126,17 @@ function changeStatus() {
 		}
 	}
 
-
 	userStatus = userStatusRadios[checkedRadio].value;
-
-	// userStatus = userStatusRadios.value;
 
 	switch (userStatus) {
 		case "available":
-			getN("presenceIconContainer").innerHTML = "<img id=\"presenceIcon\" src=\"img/availableIcon.png\">";
+			getN("presenceIconContainer").innerHTML = "<img class=\"presenceIcon\" src=\"img/availableIcon.png\">";
 			break;
 		case "unavailable":
-			getN("presenceIconContainer").innerHTML = "<img id=\"presenceIcon\" src=\"img/unavailableIcon.png\">";
+			getN("presenceIconContainer").innerHTML = "<img class=\"presenceIcon\" src=\"img/unavailableIcon.png\">";
 			break;
 		case "donotdisturb":
-			getN("presenceIconContainer").innerHTML = "<img id=\"presenceIcon\" src=\"img/donotdisturbIcon.png\">";
+			getN("presenceIconContainer").innerHTML = "<img class=\"presenceIcon\" src=\"img/donotdisturbIcon.png\">";
 			break;
 	}
 
@@ -124,16 +165,43 @@ function messageHandler(event) {
 
 		var messageData = event.data;
 		var messageDataArray = messageData.split("-");
-		var messageUser = messageDataArray[0];
+		var messageUsername = messageDataArray[0];
 		var messageType = messageDataArray[1];
 		var messageContent = messageDataArray[2];
 
 		// Handle status messages.
-		if (messageData.search("-status-") ) {
+		if (messageUsername != username && messageType == "status") {
+
+			// Check the friend's list for the username.
+			var numberOfFriends = friendList.length;
+			var i;
+
+			for (i = 0; i < numberOfFriends; i++) {
+
+				if (messageUsername == friendList[i]) {
+
+					// If they are a friend, then update their status,
+					// otherwise, ignore the message.
+
+					switch (messageContent) {
+						case "available":
+							getN("friendPresenceIconContainer" + i).innerHTML = "<img class=\"presenceIcon\" src=\"img/availableIcon.png\">";
+							break;
+						case "unavailable":
+							getN("friendPresenceIconContainer" + i).innerHTML = "<img class=\"presenceIcon\" src=\"img/unavailableIcon.png\">";
+							break;
+						case "donotdisturb":
+							getN("friendPresenceIconContainer" + i).innerHTML = "<img class=\"presenceIcon\" src=\"img/donotdisturbIcon.png\">";
+							break;
+					}
+
+
+				}
+			}
 
 		}
 
-		// Handle
+		// Handle ________________.
 
 	}
 
