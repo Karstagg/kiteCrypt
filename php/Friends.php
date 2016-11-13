8a277d9dc603f0839bd8a7564454df84e2c31630
Grant Wells <gwells4@cnm.edu>
2016-11-13 12:06:17 -0700

    Added INSERT, DELETE, and SELECT functions for the Friends class.

--- php/Friends.php
+++ php/Friends.php
@@ -3,6 +3,7 @@ namespace Edu\Cnm\kiteCrypt;
 
 require_once("autoloader.php");
 
+
 /**
  * kiteCrypt friends
  *
@@ -22,6 +23,7 @@ class Friend implements \JsonSerializable {
 	 * @var int $friendsFriendId
 	 **/
 	private $friendsFriendId;
+
 
 	/**
 	 * constructor for this Friend
@@ -49,6 +51,7 @@ class Friend implements \JsonSerializable {
 		}
 	}
 
+
 	/**
 	 * accessor method for friendsProfileId
 	 *
@@ -57,6 +60,7 @@ class Friend implements \JsonSerializable {
 	public function getFriendsProfileId() {
 		return($this->friendsProfileId);
 	}
+
 
 	/**
 	 * mutator method for friendsProfileId
@@ -90,6 +94,7 @@ class Friend implements \JsonSerializable {
 		$this->friendsProfileId = $newFriendsProfileId;
 	}
 
+
 	/**
 	 * accessor method for friendsFriendId
 	 *
@@ -98,6 +103,7 @@ class Friend implements \JsonSerializable {
 	public function getFriendsFriendId() {
 		return($this->friendsFriendId);
 	}
+
 
 	/**
 	 * mutator method for friendsFriendId
@@ -132,191 +138,193 @@ class Friend implements \JsonSerializable {
 	}
 
 
-
-
-
-
-
-
 	/**
-	 * inserts this Tweet into mySQL
+	 * inserts these Friends into mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
+	 *
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
 	public function insert(\PDO $pdo) {
-		// enforce the tweetId is null (i.e., don't insert a tweet that already exists)
-		if($this->tweetId !== null) {
-			throw(new \PDOException("not a new tweet"));
-		}
 
 		// create query template
-		$query = "INSERT INTO tweet(tweetProfileId, tweetContent, tweetDate) VALUES(:tweetProfileId, :tweetContent, :tweetDate)";
+		$query = "INSERT INTO friends(friendsProfileId, friendsFriendId) VALUES(:friendsProfileId, :friendsFriendId)";
 		$statement = $pdo->prepare($query);
 
 		// bind the member variables to the place holders in the template
-		$formattedDate = $this->tweetDate->format("Y-m-d H:i:s");
-		$parameters = ["tweetProfileId" => $this->tweetProfileId, "tweetContent" => $this->tweetContent, "tweetDate" => $formattedDate];
+		$parameters = ["friendsProfileId" => $this->friendsProfileId, "friendsFriendId" => $this->friendsFriendId];
 		$statement->execute($parameters);
 
-		// update the null tweetId with what mySQL just gave us
-		$this->tweetId = intval($pdo->lastInsertId());
 	}
 
 
 	/**
-	 * deletes this Tweet from mySQL
+	 * deletes these Friends from mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
+	 *
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
 	public function delete(\PDO $pdo) {
-		// enforce the tweetId is not null (i.e., don't delete a tweet that hasn't been inserted)
-		if($this->tweetId === null) {
-			throw(new \PDOException("unable to delete a tweet that does not exist"));
-		}
 
 		// create query template
-		$query = "DELETE FROM tweet WHERE tweetId = :tweetId";
+		$query = "DELETE FROM friends WHERE friendsProfileId = :friendsProfileId AND friendsFriendId = :friendsFriendId";
 		$statement = $pdo->prepare($query);
 
 		// bind the member variables to the place holder in the template
-		$parameters = ["tweetId" => $this->tweetId];
-		$statement->execute($parameters);
-	}
-
-	/**
-	 * updates this Tweet in mySQL
-	 *
-	 * @param \PDO $pdo PDO connection object
-	 * @throws \PDOException when mySQL related errors occur
-	 * @throws \TypeError if $pdo is not a PDO connection object
-	 **/
-	public function update(\PDO $pdo) {
-		// enforce the tweetId is not null (i.e., don't update a tweet that hasn't been inserted)
-		if($this->tweetId === null) {
-			throw(new \PDOException("unable to update a tweet that does not exist"));
-		}
-
-		// create query template
-		$query = "UPDATE tweet SET tweetProfileId = :tweetProfileId, tweetContent = :tweetContent, tweetDate = :tweetDate WHERE tweetId = :tweetId";
-		$statement = $pdo->prepare($query);
-
-		// bind the member variables to the place holders in the template
-		$formattedDate = $this->tweetDate->format("Y-m-d H:i:s");
-		$parameters = ["tweetProfileId" => $this->tweetProfileId, "tweetContent" => $this->tweetContent, "tweetDate" => $formattedDate, "tweetId" => $this->tweetId];
+		$parameters = ["friendsProfileId" => $this->friendsProfileId, "friendsFriendId" => $this->friendsFriendId];
 		$statement->execute($parameters);
 	}
 
 
 	/**
-	 * gets the Tweet by tweetId
+	 * gets the Friend by friendsProfileId
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param int $tweetId tweet id to search for
-	 * @return Tweet|null Tweet found or null if not found
+	 * @param int $friendsProfileId friendsProfileId to search for
+	 *
+	 * @return \SplFixedArray SplFixedArray of Friends found
+	 *
+	 * @throws \InvalidArgumentException if $friendsProfileId is not safe
+	 * @throws \TypeError if $friendsProfileId is not an integer
+	 * @throws \RangeException if $friendsProfileId is not positive
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when variables are not the correct data type
 	 **/
-	public static function getTweetByTweetId(\PDO $pdo, int $tweetId) {
-		// sanitize the tweetId before searching
-		if($tweetId <= 0) {
-			throw(new \PDOException("tweet id is not positive"));
+	public static function getFriendByFriendsProfileId(\PDO $pdo, int $friendsProfileId) {
+
+		// Verify the $friendsProfileId is safe
+		$friendsProfileId = filter_var($friendsProfileId, FILTER_SANITIZE_NUMBER_INT);
+		if(empty($friendsProfileId) === true) {
+			throw(new \InvalidArgumentException("friendsProfileId is empty or insecure"));
+		}
+		// Verify that the $friendsProfileId in an integer.
+		$friendsProfileId = filter_var($friendsProfileId, FILTER_VALIDATE_INT);
+		if(empty($friendsProfileId) === true) {
+			// If the $friendsProfileId is not an integer, throw a TypeError.
+			throw(new \TypeError("friendsProfileId is not an integer."));
+		}
+		// Verify the $friendsProfileId is positive
+		if($friendsProfileId <= 0) {
+			throw(new \RangeException("friendsProfileId is not positive."));
 		}
 
 		// create query template
-		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetId = :tweetId";
+		$query = "SELECT friendsFriendId FROM friends WHERE friendsProfileId = :friendsProfileId";
 		$statement = $pdo->prepare($query);
 
-		// bind the tweet id to the place holder in the template
-		$parameters = ["tweetId" => $tweetId];
+		// bind the friendsProfileId to the place holder in the template
+		$parameters = ["friendsProfileId" => $friendsProfileId];
 		$statement->execute($parameters);
 
-		// grab the tweet from mySQL
-		try {
-			$tweet = null;
-			$statement->setFetchMode(\PDO::FETCH_ASSOC);
-			$row = $statement->fetch();
-			if($row !== false) {
-				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
-			}
-		} catch(\Exception $exception) {
-			// if the row couldn't be converted, rethrow it
-			throw(new \PDOException($exception->getMessage(), 0, $exception));
-		}
-		return($tweet);
-	}
-
-	/**
-	 * gets the Tweet by profile id
-	 *
-	 * @param \PDO $pdo PDO connection object
-	 * @param int $tweetProfileId profile id to search by
-	 * @return \SplFixedArray SplFixedArray of Tweets found
-	 * @throws \PDOException when mySQL related errors occur
-	 * @throws \TypeError when variables are not the correct data type
-	 **/
-	public static function getTweetByTweetProfileId(\PDO $pdo, int $tweetProfileId) {
-		// sanitize the profile id before searching
-		if($tweetProfileId <= 0) {
-			throw(new \RangeException("tweet profile id must be positive"));
-		}
-
-		// create query template
-		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetProfileId = :tweetProfileId";
-		$statement = $pdo->prepare($query);
-
-		// bind the tweet profile id to the place holder in the template
-		$parameters = ["tweetProfileId" => $tweetProfileId];
-		$statement->execute($parameters);
-
-		// build an array of tweets
-		$tweets = new \SplFixedArray($statement->rowCount());
+		// build an array of friends
+		$friends = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while(($row = $statement->fetch()) !== false) {
 			try {
-				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
-				$tweets[$tweets->key()] = $tweet;
-				$tweets->next();
+				$friend = new Friend($row["friendsProfileId"], $row["friendsFriendId"]);
+				$friends[$friends->key()] = $friend;
+				$friends->next();
 			} catch(\Exception $exception) {
 				// if the row couldn't be converted, rethrow it
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
-		return($tweets);
+		return($friends);
 	}
+
+
+	/**
+	 * gets the Friend by friendsFriendId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $friendsFriendId friendsFriendId to search for
+	 *
+	 * @return \SplFixedArray SplFixedArray of Friends found
+	 *
+	 * @throws \InvalidArgumentException if $friendsFriendId is not safe
+	 * @throws \TypeError if $friendsFriendId is not an integer
+	 * @throws \RangeException if $friendsFriendId is not positive
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getFriendByFriendsFriendId(\PDO $pdo, int $friendsFriendId) {
+
+		// Verify the $friendsFriendId is safe
+		$friendsFriendId = filter_var($friendsFriendId, FILTER_SANITIZE_NUMBER_INT);
+		if(empty($friendsFriendId) === true) {
+			throw(new \InvalidArgumentException("friendsFriendId is empty or insecure"));
+		}
+		// Verify that the $friendsFriendId in an integer.
+		$friendsFriendId = filter_var($friendsFriendId, FILTER_VALIDATE_INT);
+		if(empty($friendsFriendId) === true) {
+			// If the $friendsFriendId is not an integer, throw a TypeError.
+			throw(new \TypeError("friendsFriendId is not an integer."));
+		}
+		// Verify the $friendsFriendId is positive
+		if($friendsFriendId <= 0) {
+			throw(new \RangeException("friendsFriendId is not positive."));
+		}
+
+		// create query template
+		$query = "SELECT friendsProfileId FROM friends WHERE friendsFriendId = :friendsFriendId";
+		$statement = $pdo->prepare($query);
+
+		// bind the friendsFriendId to the place holder in the template
+		$parameters = ["friendsFriendId" => $friendsFriendId];
+		$statement->execute($parameters);
+
+		// build an array of friends
+		$friends = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$friend = new Friend($row["friendsProfileId"], $row["friendsFriendId"]);
+				$friends[$friends->key()] = $friend;
+				$friends->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($friends);
+	}
+
 
 	/**
 	 * gets all Tweets
 	 *
 	 * @param \PDO $pdo PDO connection object
+	 *
 	 * @return \SplFixedArray SplFixedArray of Tweets found or null if not found
+	 *
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when variables are not the correct data type
 	 **/
-	public static function getAllTweets(\PDO $pdo) {
+	public static function getAllFriends(\PDO $pdo) {
 		// create query template
-		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet";
+		$query = "SELECT friendsProfileId, friendsFriendId FROM friends";
 		$statement = $pdo->prepare($query);
 		$statement->execute();
 
-		// build an array of tweets
-		$tweets = new \SplFixedArray($statement->rowCount());
+		// build an array of friends
+		$friends = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while(($row = $statement->fetch()) !== false) {
 			try {
-				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
-				$tweets[$tweets->key()] = $tweet;
-				$tweets->next();
+				$friend = new Friend($row["friendsProfileId"], $row["friendsFriendId"]);
+				$friends[$friends->key()] = $friend;
+				$friends->next();
 			} catch(\Exception $exception) {
 				// if the row couldn't be converted, rethrow it
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
-		return ($tweets);
+		return($friends);
 	}
+
 
 	/**
 	 * formats the state variables for JSON serialization
