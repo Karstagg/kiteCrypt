3bf5aba81096797e7fe63d8c8a24315c513f6a58
Jonathan Guzman <jguzman41@cnm.edu>
2016-11-14 11:57:04 -0700

    updated templates @rlewis37

--- angular2-kc/README.md
+++
@@ -1,2 +0,0 @@
-# ng-demo
-Angular 2 boilerplate project. This is a pristine Angular 2 front-end skeleton for your app. Build your own custom Services, Components and APIs.
--- angular2-kc/app/components/home-component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-import {Router} from "@angular/router";
-
-@Component({
-	templateUrl: "./templates/home.php"
-})
-
-export class HomeComponent { }
