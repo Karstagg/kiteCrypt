735b34028ba99ca615805bf578f8cd960a788eea
Jonathan Guzman <jguzman41@cnm.edu>
2016-11-14 09:55:37 -0700

    updated MySQL database file

--- sql/kitecrypt.sql
+++ sql/kitecrypt.sql
@@ -15,17 +15,19 @@ CREATE TABLE invite (
 inviteInviterId INT UNSIGNED NOT NULL,
 inviteInvitedId INT UNSIGNED NOT NULL,
 inviteTimestamp TIMESTAMP NOT NULL,
-invitePassphrase VARCHAR(32) NOT NULL,
+invitePassphrase VARCHAR(64) NOT NULL,
 UNIQUE(invitePassphrase),
 FOREIGN KEY(inviteInviterId) REFERENCES profile(profileId),
-FOREIGN KEY(inviteInvitedId) REFERENCES profile(profileId)
+FOREIGN KEY(inviteInvitedId) REFERENCES profile(profileId),
+PRIMARY KEY (inviteInviterId, inviteInvitedId)
 );
 
 CREATE TABLE friends (
 friendsProfileId INT UNSIGNED NOT NULL,
 friendsFriendId INT UNSIGNED NOT NULL,
 FOREIGN KEY(friendsProfileId) REFERENCES profile(profileId),
-FOREIGN KEY(friendsFriendId) REFERENCES profile(profileId)
+FOREIGN KEY(friendsFriendId) REFERENCES profile(profileId),
+PRIMARY KEY (friendsProfileId, friendsFriendId)
 );
 
 CREATE TABLE messages (
@@ -33,7 +35,7 @@ messageId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 messageTimestamp TIMESTAMP NOT NULL,
 messageSenderId INT UNSIGNED NOT NULL,
 messageReceiverId INT UNSIGNED NOT NULL,
-messageText VARCHAR(65485),
+messageText VARCHAR(32768),
 INDEX(messageTimestamp),
 INDEX(messageSenderId),
 INDEX(messageReceiverId),
