87e2727464f832d6cbabeb748e271cf9b4482a5b
Jon Sheafe <msckj@yahoo.com>
2016-11-15 15:14:42 -0700

    added remaining config files

---
+++ webpack/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>KiteCrypt</title>
+	</head>
+	<body>
+		<!-- This custom tag much match your Angular @Component selector name in app/app.component.ts -->
+		<kitecrypt-app>Loading&hellip;</kitecrypt-app>
+	</body>
+</html>
