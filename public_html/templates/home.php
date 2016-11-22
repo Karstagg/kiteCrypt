f641a466e1d4cf9f3ad52b3947d1471c3651a7a3
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-22 16:12:20 -0700

    fixed layout for home template

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,26 +1,29 @@
 <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 
-	<div class="container">
-		<div class="bg-image">
-				<div class="container-fluid">
-					<div id="title-row" class="row">
-						<div class="col-xs-12">
-							<div class="pad-down text-center">
-								<div id="pulse" class="pulse">
-									<h1 id="kiteCrypt-title" class="fade-in">KiteCrypt</h1>
-								</div>
-								<p id="slogan" class="fade-in-out">A secure chat site for our insecure world</p>
-								<p id="sign-up" class="pad-down2 fade-in-2">New? <a href="">Sign up!</a></p>
-							</div>
+<div class="container">
+	<div class="bg-image">
+		<div class="container-fluid">
+			<div id="title-row" class="row">
+				<div class="col-xs-12">
+					<div class="pad-down text-center">
+						<div id="pulse" class="pulse">
+							<h1 id="kiteCrypt-title" class="fade-in">KiteCrypt</h1>
 						</div>
+						<p id="slogan" class="fade-in-out">A secure chat site for our insecure world</p>
+						<p id="sign-up" class="pad-down2 fade-in-2">New? <a href="">Sign up!</a></p>
 					</div>
+				</div>
+			</div>
+			<div id="login-row" class="row">
+				<div class="text-center">
 					<form #loginForm="ngForm" name="loginForm" id="loginForm" class="form-horizontal" (ngSubmit)="login();"
 							novalidate>
 						<div class="form-group">
 							<label for="user-name">Username</label>
-							<div class="input-group">
-								<input type="text" id="user-name" class="form-control input-sm chat-input"
-										 placeholder="username" maxlength="20" required [(ngModel)]="login.username" #username="ngModel"/>
+							<div class="input-group col-xs-4 col-xs-offset-4">
+								<input type="text" id="user-name" name="user-name" class="form-control input-sm chat-input"
+										 placeholder="username" maxlength="20" required [(ngModel)]="login.username"
+										 #username="ngModel"/>
 							</div>
 							<div [hidden]="username.valid || username.pristine" class="alert alert-danger" role="alert">
 								<p *ngIf="username.errors?.required">Username is required.</p>
@@ -30,8 +33,9 @@
 						</div>
 						<div class="form-group">
 							<label for="user-password">Password</label>
-							<div class="input-group">
-								<input type="password" id="user-password" class="form-control input-sm chat-input"
+							<div class="input-group col-xs-4 col-xs-offset-4">
+								<input type="password" id="user-password" name="user-password"
+										 class="form-control input-sm chat-input"
 										 placeholder="password" required [(ngModel)]="login.password" #password="ngModel"/>
 							</div>
 							<div [hidden]="password.valid || password.pristine" class="alert alert-danger" role="alert">
@@ -39,9 +43,12 @@
 							</div>
 
 						</div>
-						<button type="submit" class="btn btn-info btn-lg" [disabled]="loginForm.invalid"><i class="fa fa-user"></i> Login</button>
+						<button type="submit" class="btn btn-info btn-lg" [disabled]="loginForm.invalid"><i
+								class="fa fa-user"></i> Login
+						</button>
 					</form>
-			<!--		main content section-->
+				</div>
+			</div>
 			<main>
 
 			</main>
