121c260d26c3489714b4d28f8b9e6793e74c89e6
Matthew Fisher <m.r.f.48215@gmail.com>
2016-11-28 10:07:56 -0700

    added new template for signup

--- public_html/templates/signup.php
+++ public_html/templates/signup.php
@@ -10,7 +10,6 @@
 							<h1 id="kiteCrypt-title" class="fade-in">KiteCrypt</h1>
 						</div>
 						<p id="slogan" class="fade-in-out">A secure chat site for our insecure world</p>
-						<p id="sign-up" class="pad-down2 fade-in-2">New? <a href="">Sign up!</a></p>
 					</div>
 				</div>
 			</div>
@@ -26,7 +25,7 @@
 										 #username="ngModel"/>
 							</div>
 							<div [hidden]="username.valid || username.pristine" class="alert alert-danger col-xs-4 col-xs-offset-4" role="alert">
-								<p *ngIf="username.errors?.required">Username required</p>
+								<p *ngIf="username.errors?.required">Username required.</p>
 								<p *ngIf="username.errors?.maxlength">Username is too long.</p>
 							</div>
 
@@ -42,6 +41,17 @@
 								<p *ngIf="password.errors?.required">Password required.</p>
 							</div>
 
+						</div>
+						<div class="form-group">
+							<label for="user-password-confirm">Password</label>
+							<div class="input-group col-xs-4 col-xs-offset-4">
+								<input type="password" id="user-password-confirm" name="user-password-confirm"
+										 class="form-control input-sm chat-input"
+										 placeholder="password" required [(ngModel)]="login.password" #password="ngModel"/>
+							</div>
+							<div [hidden]="password.valid || password.pristine" class="alert alert-danger col-xs-4 col-xs-offset-4" role="alert">
+								<p *ngIf="password.errors?.required">Password required.</p>
+							</div>
 						</div>
 						<br>
 						<button type="submit" class="btn btn-lg btn-login" [disabled]="loginForm.invalid"><i
