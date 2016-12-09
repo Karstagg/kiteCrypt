f054eef1ca3939e67d21f6c39be6b634c54e020b
Grant Wells <gwells4@cnm.edu>
2016-12-09 09:27:15 -0700

    Created jsbn-prng4.d.ts

---
+++ jsbn/jsbn-prng4.d.ts
@@ -0,0 +1,4 @@
+export function Arcfour = function () : any;
+export function ARC4init = function (key: any) : any;
+export function ARC4next = function () : any;
+export function prng_newstate = function () : any;
