faffffdd0033bfc42a52c3c02f80ac62a893a497
Jonathan Guzman <jguzman41@cnm.edu>
2016-12-09 09:38:19 -0700

    created a jsbn-rng.d.ts

--- jsbn/jsbn-rng.d.ts
+++ jsbn/jsbn-rng.d.ts
@@ -3,10 +3,6 @@
 // For best results, put code like
 // <body onClick='rng_seed_time();' onKeyPress='rng_seed_time();'>
 // in your main HTML document.
-
-export function rng_state = undefined;
-export function rng_pool = undefined;
-export function rng_pptr = undefined;
 // Mix in a 32- bit integer into the pool
 export function rng_seed_int (x: any): any;
 // Mix in the current time (w/milliseconds) into the pool
