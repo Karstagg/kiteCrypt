a508ce0e349426cd91e84e620ebe19833a3eff15
Matthew Fisher <m.r.f.48215@gmail.com>
2016-12-10 12:25:03 -0700

    added chatter box route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,12 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {SignUpComponent} from "./components/signup-component";
+import {ChatterBoxComponent} from "./components/chatter-box-component";
 
-export const allAppComponents = [HomeComponent, SignUpComponent];
+export const allAppComponents = [HomeComponent, SignUpComponent, ChatterBoxComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
-	{path: "signup", component: SignUpComponent}
+	{path: "signup", component: SignUpComponent},
+	{path: "chatter-box", component: ChatterBoxComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
