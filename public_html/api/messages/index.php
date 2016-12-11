aa1f4a8e5ce1543f11df1e9f4acb1d62ff9c2bfa
Jon Sheafe <msckj@yahoo.com>
2016-12-10 21:41:55 -0700

    API Message DELETE method added and tested successfully

--- public_html/api/messages/index.php
+++ public_html/api/messages/index.php
@@ -90,7 +90,7 @@ try {
 		if(empty($requestObject->messageText) === true) {
 			throw(new \InvalidArgumentException($exceptionMessage, $exceptionCode));
 		} else {
-			$messageText = filter_var($requestObject->messageReceiverId, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+			$messageText = filter_var($requestObject->messageText, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		}
 
 		$message = new Message(null, null, $messageSenderId, $messageReceiverId, $messageText);
@@ -101,7 +101,31 @@ try {
 //		If all username, public Key x, and public key Y match, send them to the chat page with friends list
 
 
-	} else {
+	}
+// determins if the request is a DELETE request.
+	else if($method === "DELETE") {
+//		verifyXsrf();
+
+		// retrieve the Message to be deleted
+		$Message = Message::getMessageByMessageId($pdo, $id);
+		// If the request is DELETE, the requested Message is pulled from the database using the Key in $id and stored in $Message
+
+
+		// make sure that the requested Message is valid by checking to see if $Message is null.
+		if($Message === null) {
+			throw(new RuntimeException("Message does not exist", 404));
+		}
+
+
+		// calls the DELETE method on the retrieved Message. This deletes the Message from the database.
+		$Message->delete($pdo);
+
+
+		// stores the "Message deleted OK" message in the $reply->message state variable.
+		$reply->message = "Message deleted OK";
+	}
+
+	else {
 		throw (new InvalidArgumentException("Invalid Method",400));
 	}
 
