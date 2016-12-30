284e3b1e12556fe848617bd8de2a54a0404c6842
Matthew Fisher <m.r.f.48215@gmail.com>
2016-12-30 13:05:45 -0700

    hacked and slashed :smiley_cat:

--- webpack/index.php
+++ webpack/index.php
@@ -12,7 +12,7 @@ setXsrfCookie();
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+		<base href="/" />
 
 		<title>KiteCrypt</title>
 	</head>
