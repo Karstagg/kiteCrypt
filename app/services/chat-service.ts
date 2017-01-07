fd919823ac52d40c464b0efeef8b5652280f8535
Grant Wells <gwells4@cnm.edu>
2017-01-06 17:22:53 -0700

    Added text "getMessages"

--- app/services/chat-service.ts
+++ app/services/chat-service.ts
@@ -21,7 +21,7 @@ export class ChatService extends BaseService {
 	}
 
 	getChat() : Observable<RetrieveMessages[]> {
-		return(this.http.post(this.messageUrl, "Requesting messages from server...")
+		return(this.http.post(this.messageUrl, "getMessages")
 			.map(this.extractData)
 			.catch(this.handleError));
 	}
